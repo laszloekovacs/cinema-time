df6f219514a85463173126ee35e27f1f26689916
laszloekovacs <laszloekovacs@gmail.com>
2023-11-12 18:59:57 +0100

    replaced form

--- app/components/AddShiftForm.tsx
+++ app/components/AddShiftForm.tsx
@@ -1,160 +1,55 @@
 "use client"
-import React from "react"
-import { Formik, Form, Field, FieldArray } from "formik"
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  HStack,
-  Heading,
-  Input,
-  List,
-  ListItem,
-  Select,
-  VStack,
-} from "@chakra-ui/react"
+import { Stack } from "@chakra-ui/react"
+import { useFieldArray, useForm } from "react-hook-form"
 
-const AddShiftForm = () => (
-  <div>
-    <Heading>Record a Shift</Heading>
-    <Formik
-      initialValues={{
-        employeeId: "",
-        date: "",
-        startTime: "",
-        endTime: "",
-        movies: [],
-        entry: "",
-      }}
-      onSubmit={(values) => {
-        alert(JSON.stringify(values, null, 2))
-      }}
-    >
-      {(props) => (
-        <Form>
-          <FormControl>
-            <FormLabel>Employee</FormLabel>
-            <Select
-              required
-              placeholder="pick an employee"
-              name="employeeId"
-              onChange={props.handleChange}
-              onBlur={props.handleBlur}
-              value={props.values.employeeId}
+const AddShiftForm = () => {
+  const { control, watch, register, handleSubmit, formState } = useForm({})
+  const { fields, insert, remove } = useFieldArray({
+    control,
+    name: "movies",
+  })
+  const state = watch()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  return (
+    <Stack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <select {...register("employeeId")}>
+          <option value="0">Mike</option>
+          <option value="1">Steeve</option>
+          <option value="2">Mark</option>
+        </select>
+
+        <input type="date" {...register("date")} />
+        <input type="time" {...register("start")} />
+        <input type="time" {...register("end")} />
+
+        <input type="submit" value="submit" />
+      </form>
+
+      <ul>
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <label htmlFor="title">Title</label>
+            <input type="text" {...register(`movies.${index}.title`)} />
+
+            <button
+              type="button"
+              onClick={() => {
+                remove(index)
+              }}
             >
-              <option value="1">Option 1</option>
-              <option value="2">Option 2</option>
-              <option value="3">Option 3</option>
-            </Select>
-          </FormControl>
-
-          {props.values.employeeId != "" && (
-            <>
-              <FormControl>
-                <FormLabel>Date</FormLabel>
-                <Input
-                  type="date"
-                  name="date"
-                  required
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.date}
-                />
-              </FormControl>
-
-              <FormControl>
-                <FormLabel>Start Time</FormLabel>
-                <Input
-                  type="time"
-                  name="startTime"
-                  placeholder="15:30"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.startTime}
-                />
-              </FormControl>
-
-              <FormControl>
-                <FormLabel>End Time</FormLabel>
-                <Input
-                  type="time"
-                  name="endTime"
-                  pattern="[0-9]{2}:[0-9]{2}"
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.endTime}
-                />
-                <FormErrorMessage>{props.errors.endTime}</FormErrorMessage>
-              </FormControl>
-
-              <FieldArray
-                name="movies"
-                render={(helpers) => (
-                  <>
-                    <FormLabel>Movies</FormLabel>
-                    <VStack>
-                      <List>
-                        {props.values.movies.map((movie, index) => (
-                          <ListItem key={index}>
-                            {movie}
-                            <Button onClick={() => helpers.remove(index)}>
-                              -
-                            </Button>
-                          </ListItem>
-                        ))}
-                      </List>
-                      <FormControl>
-                        <Input
-                          type="text"
-                          name="entry"
-                          onChange={props.handleChange}
-                          onBlur={props.handleBlur}
-                          value={props.values.entry}
-                        />
-                      </FormControl>
-                      <Button
-                        onClick={() => {
-                          helpers.push(props.values.entry)
-                          props.values.entry = ""
-                        }}
-                      >
-                        Add Movie
-                      </Button>
-                    </VStack>
-                  </>
-                )}
-              ></FieldArray>
-
-              <div>
-                <Button disabled={props.isSubmitting}>Submit</Button>
-              </div>
-
-              <Accordion allowToggle>
-                <AccordionItem>
-                  <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      Debug form values
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                  <AccordionPanel>
-                    <pre>{JSON.stringify(props.values, null, 2)}</pre>
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
-            </>
-          )}
-        </Form>
-      )}
-    </Formik>
-  </div>
-)
+              remove
+            </button>
+          </div>
+        ))}
+      </ul>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </Stack>
+  )
+}
 
 export default AddShiftForm
