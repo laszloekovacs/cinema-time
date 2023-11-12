f4ae3f16025ff99490c5d7b3de5de99c3871aeaa
laszloekovacs <laszloekovacs@gmail.com>
2023-11-12 17:45:21 +0100

    fixed names in form

--- app/components/AddShiftForm.tsx
+++ app/components/AddShiftForm.tsx
@@ -6,6 +6,7 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
+  HStack,
   Heading,
   Input,
   Select,
@@ -15,29 +16,77 @@ const AddShiftForm = () => (
   <div>
     <Heading>Record a Shift</Heading>
     <Formik
-      initialValues={{ employeeId: "" }}
+      initialValues={{
+        employeeId: "",
+        date: "",
+        startTime: "",
+        endTime: "",
+      }}
       onSubmit={(values) => {
         alert(JSON.stringify(values, null, 2))
       }}
     >
       {(props) => (
         <Form>
-          <Select
-            required
-            placeholder="Select employee"
-            name="employeeId"
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            value={props.values.employeeId}
-          >
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-          </Select>
+          <FormControl>
+            <FormLabel>Employee</FormLabel>
+            <Select
+              required
+              placeholder="pick an employee"
+              name="employeeId"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.employeeId}
+            >
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+            </Select>
+          </FormControl>
 
-          <FormErrorMessage>{props.errors.employeeId}</FormErrorMessage>
-          <Button disabled={props.isSubmitting}>Submit</Button>
-          <pre>{JSON.stringify(props.values, null, 2)}</pre>
+          {props.values.employeeId != "" && (
+            <>
+              <HStack>
+                <FormControl>
+                  <FormLabel>Date</FormLabel>
+                  <Input
+                    type="date"
+                    name="date"
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.date}
+                  />
+                </FormControl>
+
+                <FormControl>
+                  <FormLabel>Start Time</FormLabel>
+                  <Input
+                    name="startTime"
+                    placeholder="15:30"
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.startTime}
+                  />
+                </FormControl>
+
+                <FormControl>
+                  <FormLabel>End Time</FormLabel>
+                  <Input
+                    name="endTime"
+                    placeholder="20:00"
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    value={props.values.endTime}
+                  />
+                </FormControl>
+              </HStack>
+
+              <div>
+                <Button disabled={props.isSubmitting}>Submit</Button>
+                <pre>{JSON.stringify(props.values, null, 2)}</pre>
+              </div>
+            </>
+          )}
         </Form>
       )}
     </Formik>
