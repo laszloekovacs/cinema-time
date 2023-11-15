ad9b0cbbb6c60b428a6d4115c452cd90d30a40fa
laszloekovacs <laszloekovacs@gmail.com>
2023-11-15 10:45:56 +0100

    fixed snippets

---
+++ prisma/snippets.sql
@@ -0,0 +1,14 @@
+
+
+-- EmployeeShiftView
+CREATE OR REPLACE VIEW "EmployeeShiftView" AS 
+SELECT 
+"s"."id", 
+"s"."start", 
+"s"."end",
+"s"."date", 
+"s"."movies",
+"e"."name" AS "employee_name",
+CAST(EXTRACT(EPOCH FROM("s"."end" - "s"."start")) AS TEXT) AS "hours"
+FROM "Shift" AS "s"
+JOIN "Employee" AS "e" ON "s"."employee_id" = "e"."id";
